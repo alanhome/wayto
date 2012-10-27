acc09fb55cf41879a9350facdd5c12d069d5433d
AlanHome <niuniu2006923@gmail.com>
2012-10-27 14:44:55 +0800

    firtst

---
+++ code.py
@@ -0,0 +1,18 @@
+import web
+import os
+
+urls = (
+    '/(.*)', 'index'
+)
+
+app_root = os.path.dirname(__file__)
+templates_root = os.path.join(app_root, 'templates')
+render = web.template.render(templates_root)
+app = web.application(urls, globals())
+
+class index:        
+    def GET(self, name):
+        return render.index()
+
+if __name__ == "__main__":
+    app.run()
