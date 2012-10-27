b7513bdc863d69850452149cf4a079680010e4d0
AlanHome <niuniu2006923@gmail.com>
2012-10-28 00:30:05 +0800

    add div generator

--- code.py
+++ code.py
@@ -3,12 +3,13 @@ import web
 import os
 import json
 import weibo
-
+from weibo import APIClient
 
 urls = (
     '/$', 'index',
-	'/getpic$','getpic',
-	'/testp$', 'testp', 
+	'/getpics$','get_pics',
+	'/testp$', 'testp',
+	'/gettweets','get_tweets',
 	
 )
 
@@ -17,6 +18,15 @@ templates_root = os.path.join(app_root, 'templates')
 render = web.template.render(templates_root)
 app = web.application(urls, globals())
 
+def get_client():
+	APP_KEY = '1063723722' # app key
+	APP_SECRET = 'a4615d9d75ac60761fd07c981561b4a7' # app secret
+	client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET)
+	access_token = "2.00fzbvLCOIRzJB87e710d44be2CfhD"
+	expires_in = 360000000000
+	client.set_access_token(access_token, expires_in)
+	return client
+
 class index:        
     def GET(self):
 		x = web.input().get('query', '')
@@ -24,13 +34,32 @@ class index:
 			return render.result()
 		return render.index()
 
-class testp:
+def fill_tweet_template(jsonobj):
+	divs = ''   
+	template  = r'<div class= "row" name = "tweet" style="background-color: white;border-radius: 10px;border-width: 2px;margin-top:10px"><div class="span2" style="width:100px"><img src="%s" width="100px" height="100px" style="margin-top:5px"/><div style="text-align:center;">%s</div></div><div class="span4"><p style="font-family: 微软雅黑;font-size: 12px;margin-top:3px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;%s</p></div></div>'   # head_pic  username  text
+	tweets_list = jsonobj["statuses"]
+	for tweet in tweets_list:
+		try:
+			head_pic =  tweet["user"]["profile_image_url"].encode("utf8").replace("/50/","/180/")
+			username =  tweet["user"]["name"].encode("utf8")
+			text = tweet["text"].encode("utf8")
+			divs+= template%(head_pic,username,text)
+		except Exception:
+			continue
+	return divs
+
+class get_tweets:
 	def GET(self):
-		str = '{"statuses": [{"created_at": "Sat Oct 27 19:57:27 +0800 2012","id": "3505832429665645","mid": "3505832429665645","idstr": "3505832429665645","text": "RUCers Style http://t.cn/zl1fvbh","source": "<a href=\"http://m.weibo.com/web/cellphone.phpandroid\" rel=\"nofollow\">Android</a>","favorited": false,"truncated": false,"in_reply_to_status_id": "","in_reply_to_user_id": "","in_reply_to_screen_name": "","geo": {"type": "Point","coordinates": [39.970817,116.316032]},"user": {"id": "1647047964","idstr": "1647047964","screen_name": "angel9k5","name": "angel9k5","province": "11","city": "8","location": "","description": "","url": "","profile_image_url": "http://tp1.sinaimg.cn/1647047964/50/5600912871/0","profile_url": "u/1647047964","domain": "","weihao": "","gender": "f","followers_count": 120,"friends_count": 191,"statuses_count": 465,"favourites_count": 30,"created_at": "Sun Oct 11 17:25:05 +0800 2009","following": false,"allow_all_act_msg": false,"geo_enabled": true,"verified": false,"verified_type": -1,"remark": "","allow_all_comment": true,"avatar_large": "http://tp1.sinaimg.cn/1647047964/180/5600912871/0","verified_reason": "","follow_me": false,"online_status": 0,"bi_followers_count": 56,"lang": "zh-cn","star": 0,"level": 1,"type": 1,"ulevel": 0,"badge": {"kuainv": {"level": 0},"uc_domain": 0,"enterprise": 0,"anniversary": 0}},"reposts_count": 0,"comments_count": 0,"attitudes_count": 0,"mlevel": 0,"visible": {"type": 0,"list_id": 0},"distance": 1700}],"total_number": 6131,"states": [{"id": "3505832429665645","state": 0}]}'
-		return str
+		client = get_client()
+		jsonobj = client.get.place__poi_timeline(poiid="B2094654D16CABFE419E")
+		divs = fill_tweet_template(jsonobj)
+		return divs
 
-
-
-
+class  get_pics:
+	def GET(self):
+		client = get_client()
+		jsonobj = client.get.place__pois__photos(poiid="B2094654D16CABFE419E")
+		return jsonobj
+	
 if __name__ == "__main__":
     app.run()
